f7034d8eda040391ffc39099ef20c271a27fa238
Wisnu Wardana <wisnu.wardana@allianz.co.id>
2021-08-21 14:15:54 +0700

    Initial commit

---
+++ movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movs
+//
+//  Created by wisnu wardhana on 21/08/21.
+//  Copyright © 2021 WIZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
